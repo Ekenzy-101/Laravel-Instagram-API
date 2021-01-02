f2a02a46398fa20b9a08ade0ecbed0f9a557e05e
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-01-02 21:59:09 +0100

    Configured and handled error for JWT

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,11 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -36,5 +40,24 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // detect instance
+        if ($exception instanceof UnauthorizedHttpException) {
+            // detect previous instance
+            if ($exception instanceof TokenExpiredException) {
+                return response()->json(['message' => 'Expired Token'], $exception->getStatusCode());
+            } else if ($exception instanceof TokenInvalidException) {
+                return response()->json(['message' => 'Invalid Token'], $exception->getStatusCode());
+            } else if ($exception instanceof TokenBlacklistedException) {
+                return response()->json(['message' =>'Blacklisted Token'], $exception->getStatusCode());
+            } else {
+                return response()->json(['message' => 'Unauthorized'], 401);
+            }
+        }
+
+        return parent::render($request, $exception);
     }
 }
