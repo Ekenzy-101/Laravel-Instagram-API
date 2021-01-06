91305949396709b5deac96128701ba0b30ad648a
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-01-06 09:44:17 +0100

    Added view for verify email template

---
+++ resources/views/emails/verify-email.blade.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <body>
+    <div style="margin: 0; padding: 0" dir="ltr" bgcolor="#ffffff">
+      <table
+        border="0"
+        cellspacing="0"
+        cellpadding="0"
+        align="center"
+        id="m_-8360065665752195467email_table"
+        style="border-collapse: collapse"
+      >
+        <tbody>
+          <tr>
+            <td
+              id="m_-8360065665752195467email_content"
+              style="
+                font-family: Helvetica Neue, Helvetica, Lucida Grande, tahoma,
+                  verdana, arial, sans-serif;
+                background: #ffffff;
+              "
+            >
+              <table
+                border="0"
+                width="100%"
+                cellspacing="0"
+                cellpadding="0"
+                style="border-collapse: collapse"
+              >
+                <tbody>
+                  <tr>
+                    <td height="20" style="line-height: 20px" colspan="3">
+                      &nbsp;
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="1" colspan="3" style="line-height: 1px"></td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table
+                        border="0"
+                        width="100%"
+                        cellspacing="0"
+                        cellpadding="0"
+                        style="
+                          border-collapse: collapse;
+                          border: solid 1px white;
+                          margin: 0 auto 5px auto;
+                          padding: 3px 0;
+                          text-align: center;
+                          width: 430px;
+                        "
+                      >
+                        <tbody>
+                          <tr>
+                            <td width="15px" style="width: 15px"></td>
+                            <td
+                              style="
+                                line-height: 0px;
+                                width: 400px;
+                                padding: 0 0 15px 0;
+                              "
+                            >
+                              <table
+                                border="0"
+                                cellspacing="0"
+                                cellpadding="0"
+                                style="border-collapse: collapse"
+                              >
+                                <tbody>
+                                  <tr>
+                                    <td
+                                      style="
+                                        width: 100%;
+                                        text-align: left;
+                                        height: 33px;
+                                      "
+                                    >
+                                      <img
+                                        height="33"
+                                        src="https://ci4.googleusercontent.com/proxy/H-WMBt20rSRWwIK0YLwC8Uyi1mnXWEEEiUT0twBA78N4_Rbri9VuqAL_Azd6xVjLkSiTQ6r1RjyDJ2Hx1O_3UqLo4H_LxG1o8LHL4yDfZw=s0-d-e1-ft#https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/QTa-gpOyYBa.png"
+                                        style="border: 0"
+                                        class="CToWUd"
+                                      />
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                            <td width="15px" style="width: 15px"></td>
+                          </tr>
+                          <tr>
+                            <td width="15px" style="width: 15px"></td>
+                            <td style="border-top: solid 1px #dbdbdb"></td>
+                            <td width="15px" style="width: 15px"></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table
+                        border="0"
+                        width="430"
+                        cellspacing="0"
+                        cellpadding="0"
+                        style="border-collapse: collapse; margin: 0 auto 0 auto"
+                      >
+                        <tbody>
+                          <tr>
+                            <td>
+                              <table
+                                border="0"
+                                width="430px"
+                                cellspacing="0"
+                                cellpadding="0"
+                                style="
+                                  border-collapse: collapse;
+                                  margin: 0 auto 0 auto;
+                                  width: 430px;
+                                "
+                              >
+                                <tbody>
+                                  <tr>
+                                    <td
+                                      width="15"
+                                      style="display: block; width: 15px"
+                                    >
+                                      &nbsp;&nbsp;&nbsp;
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td
+                                      width="12"
+                                      style="display: block; width: 12px"
+                                    >
+                                      &nbsp;&nbsp;&nbsp;
+                                    </td>
+                                    <td>
+                                      <table
+                                        border="0"
+                                        width="100%"
+                                        cellspacing="0"
+                                        cellpadding="0"
+                                        style="border-collapse: collapse"
+                                      >
+                                        <tbody>
+                                          <tr>
+                                            <td></td>
+                                            <td
+                                              style="
+                                                margin: 10px 0 10px 0;
+                                                color: #565a5c;
+                                                font-size: 18px;
+                                              "
+                                            >
+                                              <p
+                                                style="
+                                                  margin: 10px 0 10px 0;
+                                                  color: #565a5c;
+                                                  font-size: 18px;
+                                                "
+                                              >
+                                                Hi,
+                                              </p>
+                                              <p
+                                                style="
+                                                  margin: 10px 0 10px 0;
+                                                  color: #565a5c;
+                                                  font-size: 18px;
+                                                "
+                                              >
+                                                Someone tried to sign up for an
+                                                Instagram account with
+                                                <a
+                                                  href="mailto:{{$user->email}}"
+                                                  target="_blank"
+                                                  >{{$user->email}}</a
+                                                >. If it was you, enter this
+                                                confirmation code in the app:
+                                              </p>
+                                            </td>
+                                          </tr>
+                                          <tr>
+                                            <td></td>
+                                            <td
+                                              style="
+                                                padding: 10px;
+                                                color: #565a5c;
+                                                font-size: 32px;
+                                                font-weight: 500;
+                                                text-align: center;
+                                                padding-bottom: 25px;
+                                              "
+                                            >
+                                              {{$user->verification_code ?? 555444}}
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table
+                        border="0"
+                        width="430px"
+                        cellspacing="0"
+                        cellpadding="0"
+                        style="
+                          border-collapse: collapse;
+                          margin: 0 auto 0 auto;
+                          width: 430px;
+                        "
+                      >
+                        <tbody>
+                          <tr>
+                            <td height="5" style="line-height: 5px" colspan="3">
+                              &nbsp;
+                            </td>
+                          </tr>
+                          <tr>
+                            <td width="20" style="display: block; width: 20px">
+                              &nbsp;&nbsp;&nbsp;
+                            </td>
+                            <td>
+                              <div style="padding-top: 10px">
+                                <img
+                                  src="https://ci4.googleusercontent.com/proxy/1jVmGWy9tCnCqBWLSinJ6Z8m-mANhlu-0HJJpn3x1Rf1YzMg3CCnm8YzpKQh29yaES9XHM9NySfBVkv1HDbly59FbBb3QtlImd0tFZxpVA=s0-d-e1-ft#https://static.xx.fbcdn.net/rsrc.php/v3/yP/r/ARZq-vP6uSX.png"
+                                  height="30"
+                                  width="77"
+                                  alt=""
+                                  class="CToWUd"
+                                /><br />
+                              </div>
+                              <div style="height: 10px"></div>
+                              <div
+                                style="
+                                  color: #abadae;
+                                  font-size: 11px;
+                                  margin: 0 auto 5px auto;
+                                "
+                              >
+                                © Instagram. Facebook Inc., 1601 Willow Road,
+                                Menlo Park, CA 94025<br />
+                              </div>
+                              <div
+                                style="
+                                  color: #abadae;
+                                  font-size: 11px;
+                                  margin: 0 auto 5px auto;
+                                "
+                              >
+                                This message was sent to
+                                <a
+                                  style="
+                                    color: #abadae;
+                                    text-decoration: underline;
+                                  "
+                                  >{{$user->email}}</a
+                                >.<br />
+                              </div>
+                            </td>
+                            <td width="20" style="display: block; width: 20px">
+                              &nbsp;&nbsp;&nbsp;
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="20" style="line-height: 20px" colspan="3">
+                      &nbsp;
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <span
+                ><img
+                  src="https://ci4.googleusercontent.com/proxy/Ykca0hkIuHaJglaUSC-OFHUl1VL25eWW__9bKppDQmg8NC5DYAgap6x9ADRO8D8TYK178Cc1N3DGhuA7yKxpeuQuNrKjHOAzsg7S_w3jLBMADEQfMW545con8iAoE3IfijwkZs8ZOAp7m5hO0LNiNYedHUfS=s0-d-e1-ft#https://www.facebook.com/email_open_log_pic.php?mid=5b7d67befafbbG24bc2cdafa4000G5b7d6c585b28eG37f"
+                  style="border: 0; width: 1px; height: 1px"
+                  class="CToWUd"
+              /></span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
