4aa5cb90106903d5f894cb45f5fbc15f21ee9dcb
Ekenzy-101 <ekeneonyekaba@gmail.com>
2021-02-15 09:14:28 +0100

    Initial Deploy

--- config/app.php
+++ config/app.php
@@ -173,8 +173,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\HelperServiceProvider::class,
-
     ],
 
     /*
